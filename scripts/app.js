2a2d0f8ef90bda9429015fccba103f29cc677341
Ptolemaik <cryptolemaik@gmail.com>
2015-01-08 06:49:01 -0500

    Add grid, button scripts

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,20 @@
 $(document).ready(function() {
 
+	for (var i = 0; i < 256; i++)
+		$('#container').append('<div class="gridbox"></div>');
 
+	$('.gridbox').click(function() {
+		$(this).fadeOut('slow', 0);
+	});
 
-	
+	$('.gridbox').hover(function() {
+		$(this).css("background-color", "blue");
+	}, function() {
+		//$(this).css("background-color", "white");
+	});
+
+	$('button').click(function() {
+		$('.gridbox').css("background-color", "white");
+	})
+
 });
