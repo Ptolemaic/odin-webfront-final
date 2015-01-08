41f1537120bfdfd25dd1190a0c9d5709363fe010
JN <naughton.joshua@gmail.com>
2015-01-08 11:18:51 -0500

    Refactor input panel, button events

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
 
-	var blank = true;
+	var rows = $('input[name=\'rows\']').val();
+	var cols = $('input[name=\'cols\']').val();
+	var boxes = $('input[name=\'boxes\']').val();
 
 	for (var i = 0; i < 1024; i++)
 		$('#container').append('<div class="gridbox"></div>');
@@ -15,20 +17,49 @@ $(document).ready(function() {
 		//$(this).css("background-color", "white");
 	});
 
-	$('button').click(function() {
-		$('.gridbox').css("background-color", "white");
+	$('#clear')
+		.mousedown(function() {
+			$('.gridbox').css("background-color", "white");
+			$(this).css("background-color", "#66FF99");
+		})
+		.mouseup(function() {
+			$(this).css("background-color", "#555555");
 	});
 
-	$('#clear').hover(function() {
+	$('#reset')
+		.mousedown(function() {
+			$('input[name=\'rows\']').val(0);
+			$('input[name=\'cols\']').val(0);
+			$('input[name=\'boxes\']').val(0);
+			$(this).css("background-color", "#66FF99");
+		})
+		.mouseup(function() {
+			$(this).css("background-color", "#555555");
+	});
+
+	$('button').hover(function() {
 		$(this).css("background-color", "#666666");
-		//$(this).css("color", "#555555");
 	}, function() {
 		$(this).css("background-color", "#555555");
 		$(this).css("color", "#DDDDDD");
 	});
 
-	$('#clear').click(function() {
-		$(this).css("background-color", "green");
+	$('#reset').click(function() {
+		console.log();
 	});
 
+	$('input[name=\'rows\']').change(function() {
+		var rows = $('input[name=\'rows\']').val();
+		console.log(rows);
+	});
+
+	$('input[name=\'cols\']').change(function() {
+		var cols = $('input[name=\'cols\']').val();
+		console.log(cols);
+	});
+
+	$('input[name=\'boxes\']').change(function() {
+		var boxes = $('input[name=\'boxes\']').val();
+		console.log(boxes);
+	});
 });
